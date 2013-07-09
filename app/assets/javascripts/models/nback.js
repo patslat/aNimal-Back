33ef2c88ed10e9886d7ff86fb0ee00953426db39
Patrick Slattery <slattery.patrickj@gmail.com>
2013-07-09 11:50:42 -0700

    move lots of logic to model

--- app/assets/javascripts/models/nback.js
+++ app/assets/javascripts/models/nback.js
@@ -1,17 +1,53 @@
 Nback.Models.Nback = Backbone.Model.extend({
 
-  generateSequence: function () {
-    var sequences = [];
+  initialize: function () {
+    this.sequences = [];
+    this.currentResponse = [false, false];
+    this.accuracy = [];
+  },
+
+  generateSequences: function () {
+    this.sequences = [];
     var blocks = [];
     var sounds = ["A", "B", "C", "D", "E", "F", "G", "H"];
     for (var i = 0; i < 20; i++) {
       var block = Math.floor(Math.random() * 8);
       var sound = sounds[Math.floor(Math.random() * 8)];
 
-      sequences.push([block, sound]);
+      this.sequences.push([block, sound]);
     }
+  },
 
-    return sequences;
+  endPass: function (i) {
+    var correct = this._getCorrectInput(i);
+    var response = this.currentResponse;
+    this.accuracy.push(
+      (correct[0] === response[0]) && (correct[1] === response[1])
+    )
+  },
+
+  wonRound: function () {
+    var accuracy =
+      _(this.accuracy).select(function (acc) {
+        if (acc) return acc;
+      }).length;
+
+    return (accuracy / this.sequences.length > .75);
+  },
+
+  _getCorrectInput: function (i) {
+    if ((i + this.n) < this.sequences.length) {
+      var currentVisual = this.sequences[i][0];
+      var currentAuditory = this.sequences[i][1];
+
+      var nbackVisual = this.sequences[i + this.n][0];
+      var nbackAuditory = this.sequences[i + this.n][1];
+    }
+    return [
+      (currentVisual === nbackVisual),
+      (currentAuditory === nbackAuditory)
+    ];
+
   }
 
 });
--- app/assets/javascripts/views/nbacks/nbacks_index.js
+++ app/assets/javascripts/views/nbacks/nbacks_index.js
@@ -1,7 +1,7 @@
 Nback.Views.NbacksIndex = Backbone.View.extend({
 
   initialize: function () {
-
+    this.game = new Nback.Models.Nback();
   },
 
   template: JST['nbacks/index'],
@@ -16,34 +16,47 @@ Nback.Views.NbacksIndex = Backbone.View.extend({
     return this;
   },
 
-  play: function () {
+  play: function (n) {
     var self = this
-    var game = new Nback.Models.Nback();
-    var sequence = game.generateSequence();
+    this.game.n = n || 1
+    this.game.generateSequences();
 
     (function gameLoop (i) {
+      self.game.response = [false, false];
+
       self._bindInputs();
 
-      var block = sequence[i - 1][0];
-      var sound = sequence[i - 1][1];
+      var block = self.game.sequences[i - 1][0];
+      var sound = self.game.sequences[i - 1][1];
 
       setTimeout(function () {
-        $.playSound("sounds/" + sound + ".mp3")
+        $.playSound("sounds/" + sound + ".mp3");
         $("#block-" + block).toggleClass("light");
       }, 1000)
       setTimeout(function () {
         $("#block-" + block).toggleClass("light");
         self._unbindInputs();
-        if (--i) gameLoop(i);
+
+        self.game.endPass(i - 1)
+
+        if (--i) {
+          gameLoop(i)
+        } else {
+          if (self.game.wonRound()) {
+            console.log("Winner Winner Chicken Dinner");
+            self.play(n + 1);
+          } else {
+            console.log("You lost at n = " + this.game.n);
+          }
+        }
       }, 2000)
 
-    })(sequence.length)
-
+    })(this.game.sequences.length);
   },
 
   _bindInputs: function () {
-    _.bindAll(this)
-    $(document).on("keypress", this._registerResponse)
+    _.bindAll(this);
+    $(document).on("keypress", this._registerResponse);
   },
 
   _unbindInputs: function () {
@@ -52,15 +65,17 @@ Nback.Views.NbacksIndex = Backbone.View.extend({
 
 
   _registerResponse: function (event) {
-    this._giveFeedback(event.keyCode)
+    this._giveFeedback(event.keyCode);
   },
 
   _giveFeedback: function (key) {
-    // a === 97; l === 108
+    // auditory === 97; visual === 108
     if (key === 97) {
-      this._indicate($("#location-indicator"))
+      this.game.currentResponse[1] = true;
+      this._indicate($("#location-indicator"));
     } else if (key === 108) {
-      this._indicate($("#auditory-indicator"))
+      this.game.currentResponse[0] = true;
+      this._indicate($("#auditory-indicator"));
     }
   },
 
