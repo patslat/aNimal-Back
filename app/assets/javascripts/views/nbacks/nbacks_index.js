c228c63404e6f7cae80fec8b82edfcda5c7715b9
Patrick Slattery <slattery.patrickj@gmail.com>
2013-07-09 10:58:41 -0700

    moved functions out of loop

--- app/assets/javascripts/views/nbacks/nbacks_index.js
+++ app/assets/javascripts/views/nbacks/nbacks_index.js
@@ -1,9 +1,13 @@
 Nback.Views.NbacksIndex = Backbone.View.extend({
 
+  initialize: function () {
+
+  },
+
   template: JST['nbacks/index'],
 
   events: {
-    "click #start": "play"
+    "click #start": "play",
   },
 
   render: function () {
@@ -13,10 +17,13 @@ Nback.Views.NbacksIndex = Backbone.View.extend({
   },
 
   play: function () {
+    var self = this
     var game = new Nback.Models.Nback();
     var sequence = game.generateSequence();
 
     (function gameLoop (i) {
+      self._bindInputs();
+
       var block = sequence[i - 1][0];
       var sound = sequence[i - 1][1];
 
@@ -26,11 +33,42 @@ Nback.Views.NbacksIndex = Backbone.View.extend({
       }, 1000)
       setTimeout(function () {
         $("#block-" + block).toggleClass("light");
+        self._unbindInputs();
         if (--i) gameLoop(i);
       }, 2000)
 
     })(sequence.length)
-  }
 
+  },
+
+  _bindInputs: function () {
+    _.bindAll(this)
+    $(document).on("keypress", this._registerResponse)
+  },
+
+  _unbindInputs: function () {
+    $(document).off('keypress', this._registerResponse);
+  },
+
+
+  _registerResponse: function (event) {
+    this._giveFeedback(event.keyCode)
+  },
+
+  _giveFeedback: function (key) {
+    // a === 97; l === 108
+    if (key === 97) {
+      this._indicate($("#location-indicator"))
+    } else if (key === 108) {
+      this._indicate($("#auditory-indicator"))
+    }
+  },
+
+  _indicate: function ($el) {
+    $el.toggleClass("light");
+    setTimeout( function () {
+      $el.toggleClass("light")
+    }, 200);
+  },
 
 });
